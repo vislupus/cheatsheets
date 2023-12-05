8a2224d552113435fa3cbf87b8a066d6edc5b02c
Nikola Bozhinov <nikolaobozhinov@gmail.com>
2023-12-05 08:34:50 +0200

    update class

--- python/class.py
+++ python/class.py
@@ -11,7 +11,7 @@ class Dog:
 
     def Sleep(self):
         if self.Tired == True:
-            return 'I will sleep'
+            return "I will sleep"
         else:
             return "I don't want to sleep"
 
@@ -19,23 +19,24 @@ class Dog:
         self.tricks.append(trick)
 
 
-d = Dog('Fido', 'Husky', 15, tired=True)
+d = Dog("Fido", "Husky", 15, tired=True)
 print(d)
 print(d.Sleep())
-d.add_trick('roll over')
+d.add_trick("roll over")
 print(d.tricks)
 
-e = Dog('Buddy', 'Labrador', 5, tired=False)
+e = Dog("Buddy", "Labrador", 5, tired=False)
 print(e)
 print(e.Sleep())
-e.add_trick('play dead')
+e.add_trick("play dead")
 print(e.tricks)
 
 
 class Cat(Dog):
     "This is a cat class"
-    play=True
-    def __init__(self, name, breed, age, tired, furry, season='summer'):
+    play = True
+
+    def __init__(self, name, breed, age, tired, furry, season="summer"):
         Dog.__init__(self, name, breed, age, tired)
         self.Furry = furry
         self.Season = season
@@ -48,23 +49,23 @@ class Cat(Dog):
         return self.Season
 
     def __del__(self):
-        print('Destructor called')
+        print("Destructor called")
 
-c = Cat('Mata', 'Tcbby', 13, tired=False, furry=True)
+
+c = Cat("Mata", "Tcbby", 13, tired=False, furry=True)
 print(c)
 print(c.__doc__)
 print(c.play)
-print(c.season('fall'))
+print(c.season("fall"))
 print(c.Sleep())
-c.add_trick('sleep')
+c.add_trick("sleep")
 print(c.tricks)
 
 
 print(isinstance(c, Cat))
-print(issubclass(Cat,Dog))
+print(issubclass(Cat, Dog))
 
 del c
-
 
 
 class Point:
@@ -81,43 +82,45 @@ class Point:
         return Point(x, y)
 
     def __mul__(self, other):
-        x = self.x*other.x
-        y = self.y*other.y
+        x = self.x * other.x
+        y = self.y * other.y
         return Point(x, y)
 
     def __eq__(self, other):
-        self_mag = (self.x ** 2) + (self.y ** 2)
-        other_mag = (other.x ** 2) + (other.y ** 2)
+        self_mag = (self.x**2) + (self.y**2)
+        other_mag = (other.x**2) + (other.y**2)
         return self_mag == other_mag
 
     def __lt__(self, other):
-        self_mag = (self.x ** 2) + (self.y ** 2)
-        other_mag = (other.x ** 2) + (other.y ** 2)
+        self_mag = (self.x**2) + (self.y**2)
+        other_mag = (other.x**2) + (other.y**2)
         return self_mag < other_mag
+
 
 p1 = Point(1, 2)
 print(p1)
 p2 = Point(2, 3)
 print(p2)
 
-print(p1+p2)
-print(p1*p2)
-print(p1==p2)
-print(p1<p2)
+print(p1 + p2)
+print(p1 * p2)
+print(p1 == p2)
+print(p1 < p2)
 
 
-
-class Test():
+class Test:
     def _decorator(func):
         def func_wrapper(self, x):
             print(f"start magic {x} - {func.__name__}")
-            func(self, x*2)
+            func(self, x * 2)
             print(f"end magic {x} - {func.__name__}")
+
         return func_wrapper
 
     @_decorator
     def bar(self, n):
         print(f"normal call {n}")
+
 
 test = Test()
 test.bar(3)
